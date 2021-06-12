e03017976cf8363939a31d5fb0ce338283392f9c
ahmad-irfaan <irfaan.hibatullah@gmail.com>
2021-06-13 06:43:15 +0700

    notes back-end

---
+++ src/services/inMemory/NotesService.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable linebreak-style */
+class NotesService {
+  constructor() {
+    this._notes = [];
+  }
+}
