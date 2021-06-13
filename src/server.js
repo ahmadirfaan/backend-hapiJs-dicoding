fa197742548bd31c316581d5b3a7f8d24d9f5b3d
ahmad-irfaan <irfaan.hibatullah@gmail.com>
2021-06-13 08:40:24 +0700

    Succes test run postman

--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
-/* eslint-disable linebreak-style */
+// mengimpor dotenv dan menjalankan konfigurasinya
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
-const routes = require('./api/notes/routes');
+const songs = require('./api/songs');
+const SongsService = require('./services/postgres/SongsService');
+const SongsValidator = require('./validator/songs');
 
 const init = async () => {
+  const songsService = new SongsService();
   const server = Hapi.server({
-    port: 5000,
-    host: 'localhost',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
@@ -13,7 +18,13 @@ const init = async () => {
     },
   });
 
-  server.route(routes);
+  await server.register({
+    plugin: songs,
+    options: {
+      service: songsService,
+      validator: SongsValidator,
+    },
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
--- src/services/inMemory/NotesService.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-underscore-dangle */
-/* eslint-disable linebreak-style */
-class NotesService {
-  constructor() {
-    this._notes = [];
-  }
-}
